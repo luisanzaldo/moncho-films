23acc61ee525e2b02b16aab2373bb7dd803db9ff
luisanzaldo <luis.anzaldo@metarrelato.com>
2026-01-15 20:24:10 -0600

    Update implementation

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/moncho-films/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        criticas: resolve(__dirname, 'criticas.html'),
+      },
+    },
+  },
 })
